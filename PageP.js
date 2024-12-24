ff8c65bf41606d8315fc466d409c80fee4fa1152
ikram <youremail@example.com>
2024-12-24 22:37:45 +0100

    Page principale 5

--- PageP.js
+++ PageP.js
@@ -10,6 +10,9 @@ les_categories.forEach((listItem, index) => {
     
         les_categories_names[index].classList.remove('innactive');
         les_categories_names[index].classList.add('active');
+les_categories_names[index].addEventListener('click',()=>{
+  
+        })   
     });
 
     listItem.addEventListener('mouseleave', () => {
@@ -27,4 +30,47 @@ var search = document.querySelector('.searchBar input')
 search.addEventListener('keyup',function(e){
     if(e.key==='Enter')
       console.log(this.value)
+})
+
+var searchIcon = document.querySelector('.searchBar i')
+searchIcon.addEventListener('click',()=>{
+   console.log(search.value) 
+})
+// add a new poster 
+
+var addPost = document.querySelector('.addPost')
+var newPost = document.querySelector('.newPost')
+addPost.addEventListener('click',()=>{
+  newPost.classList.remove('innactive')
+  newPost.classList.add('active')
+  addPost.classList.remove('active')
+  addPost.classList.add('innactive')
+  
+})
+
+var newpostAddB = document.querySelector('.AddButton')
+var newpostBackB = document.querySelector('.BackButton')
+const Textarea = document.querySelector('.newPost textarea')
+
+newpostBackB.addEventListener('click',()=>{
+    newPost.classList.remove('active')
+    newPost.classList.add('innactive')
+    addPost.classList.remove('innactive')
+    addPost.classList.add('active')
+    Textarea.value=''
+})
+newpostAddB.addEventListener('click',()=>{
+    if(Textarea.value!==''){
+    newPost.classList.remove('active')
+    newPost.classList.add('innactive')
+    addPost.classList.remove('innactive')
+    addPost.classList.add('active')
+    Textarea.value=''
+}
+})
+
+
+//help
+document.querySelector('.help').addEventListener('click',()=>{
+    window.location.href='./help.html'
 })
