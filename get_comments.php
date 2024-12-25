b6c96f5037d3a0e15e22dbbb9acff919f080a3f8
rayhane <youremail@example.com>
2024-12-25 19:21:10 +0100

    k

--- get_comments.php
+++ get_comments.php
@@ -1,34 +1,27 @@
 <?php
-session_start();
-if (!isset($_SESSION['user_id'])) {
-  echo "Vous devez être connecté pour voir les commentaires.";
-  exit();
-}
-
 // Connexion à la base de données
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "root;
+$dbname = "root";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
-  $user_id = $_SESSION['user_id'];
-
-  $sql = "SELECT comment FROM comments WHERE user_id = '$user_id'";
-  $result = $conn->query($sql);
-
-  $comments = [];
-  if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-      $comments[] = $row;
-    }
-  }
-
-  echo json_encode($comments);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
+
+$sql = "SELECT comments.comment, users.username FROM comments JOIN users ON comments.user_id = users.id";
+$result = $conn->query($sql);
+
+$comments = [];
+if ($result->num_rows > 0) {
+  while ($row = $result->fetch_assoc()) {
+    $comments[] = $row;
+  }
+}
+
+echo json_encode($comments);
 
 $conn->close();
 ?>
