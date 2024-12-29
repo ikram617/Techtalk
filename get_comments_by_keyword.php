7c1874a33d5eeb4cc0c173f0320ae54c2f7b0950
rayhane <youremail@example.com>
2024-12-29 23:05:19 +0100

    cbn

--- get_comments_by_keyword.php
+++ get_comments_by_keyword.php
@@ -13,12 +13,15 @@ if ($conn->connect_error) {
   die(json_encode(['error' => 'Connection failed: ' . $conn->connect_error]));
 }
 
+$keyword = isset($_GET['keyword']) ? $conn->real_escape_string($_GET['keyword']) : '';
+
 $sql = "
   SELECT comments.idc AS comment_id, users.username, users.field AS userField, comments.comment, comments.created_at,
          (SELECT COUNT(*) FROM comments_likes WHERE comments_likes.comment_id = comments.idc AND comments_likes.type = 'like') AS likes,
          (SELECT COUNT(*) FROM comments_likes WHERE comments_likes.comment_id = comments.idc AND comments_likes.type = 'dislike') AS dislikes
   FROM comments
   JOIN users ON comments.user_id = users.id
+  WHERE comments.comment LIKE '%$keyword%'
   ORDER BY comments.created_at DESC
 ";
 
